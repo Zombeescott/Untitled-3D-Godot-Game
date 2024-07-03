46a353bbf3874d9a67094ddf7512de9a723d9e3f
Scott Ford <zombeescott@gmail.com>
2024-07-03 00:37:50 -0700

    Fixed player's shadow interacting with areas by giving bubble a collision

--- Components/jumppad_component.gd
+++ Components/jumppad_component.gd
@@ -5,6 +5,7 @@ class_name JumppadComponent
 @export var type: String
 @export var strength: float
 @export var pop_timer: Timer
+@export var collision: CollisionShape3D
 
 
 func _on_body_entered(body: Node3D) -> void:
@@ -16,6 +17,8 @@ func _on_body_entered(body: Node3D) -> void:
 			self.visible = false
 			self.set_deferred("monitoring", false)
 			self.set_collision_layer_value(1, false)
+			if collision:
+				collision.set_deferred("disabled", true)
 			pop_timer.start()
 
 
@@ -23,3 +26,5 @@ func _on_timer_timeout() -> void:
 	self.visible = true
 	self.set_deferred("monitoring", true)
 	self.set_collision_layer_value(1, true)
+	if collision:
+		collision.set_deferred("disabled", false)
